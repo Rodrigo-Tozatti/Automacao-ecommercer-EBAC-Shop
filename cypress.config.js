8c0d7fec39f3cc8fe6d5f542f6d562bad79ad07c
Rodrigo_Tozatti <rodrigo.tozatti@hotmail.com>
2023-06-10 01:23:18 -0300

    Automacao ecommerce concluido

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'yxwyr5',
+  projectId: 'o5cvxh',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
