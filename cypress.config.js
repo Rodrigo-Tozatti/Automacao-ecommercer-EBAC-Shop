142f2dd9080af7058c3d3174b8ed408ebbb279b8
Rodrigo_Tozatti <rodrigo.tozatti@hotmail.com>
2023-06-11 01:23:58 -0300

    Relatório Mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,14 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },reporterOptions: {
+      reportDir: "cypress/report/mochawesome-report",
+      overwrite: true,
+      html: true,
+      json: false,
+      timestamp: "mmddyyyy_HHMMss",
     },
+
     baseUrl: 'http://lojaebac.ebaconline.art.br/'
   },
 });
